75c3bccf0e93622ac0e02f5b6a0b743bda4e4607
Nikrad Mahdi <1885199+nikrad@users.noreply.github.com>
2024-02-06 19:53:16 -0800

    Update hash

--- main.b2178ad5.js
+++ main.b2178ad5.js
@@ -60,7 +60,7 @@ function daysSinceJanFirst2024PT() {
 // Function to fetch words from JSON file
 async function loadWords() {
   try {
-    const response = await fetch("words.c9579cd7.json");
+    const response = await fetch("words.b2178ad5.json");
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
